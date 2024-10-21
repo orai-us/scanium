4b3f64d51d9b76f6109cc2982dbbde6954bc4558
NxHuan <huan.nx@orai.io>
2024-10-21 14:47:48 +0700

    update: add UI for contract detail

---
+++ src/modules/[chain]/contracts/[address].vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import ContractDetail from '@/components/contracts/ContractDetail.vue';
+import TransactionContract from '@/components/contracts/TransactionContract.vue';
+
+</script>
+<template>
+    <ContractDetail />
+    <TransactionContract />
+</template>
