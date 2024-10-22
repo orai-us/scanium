34b1a061b4d3c25ddb486caec0ed302f13e2b682
NxHuan <huan.nx@orai.io>
2024-10-22 16:59:50 +0700

    update: handle config pagination for txs contract

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,49 @@
+// import { createApolloProvider } from '@vue/apollo-option';
 import '@oraichain/oraiscan-widget';
 import App from '@/App.vue';
 import i18n from '@/plugins/i18n';
 import '@/style.css';
-import { createApp, ref } from 'vue';
+import { createApp, h, provide, ref } from 'vue';
 import { createPinia } from 'pinia';
 import LazyLoad from 'lazy-load-vue3';
+import VueAwesomePaginate from 'vue-awesome-paginate';
+import 'vue-awesome-paginate/dist/style.css';
 
 import router from './router';
 import { useBaseStore } from './stores/useBaseStore';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+
+// apollo
+const cache = new InMemoryCache();
+
+const httpLink = createHttpLink({
+  uri: 'http://192.168.10.63:3000',
+})
+
+const apolloClient = new ApolloClient({
+  cache,
+  link: httpLink,
+});
+
+// const apolloProvider = createApolloProvider({
+//   defaultClient: apolloClient,
+// });
 
 // Create vue app
-const app = createApp(App);
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
 // Use plugins
 app.use(i18n);
 app.use(createPinia());
 app.use(router);
 app.use(LazyLoad, { component: true });
+app.use(VueAwesomePaginate as any);
+// app.use(apolloProvider);
 // Mount vue app
 app.mount('#app');
 
--- src/modules/[chain]/contracts/[address].vue
+++ src/modules/[chain]/contracts/[address].vue
@@ -2,8 +2,10 @@
 import ContractDetail from '@/components/contracts/ContractDetail.vue';
 import TransactionContract from '@/components/contracts/TransactionContract.vue';
 
+const props = defineProps(['chain'])
+
 </script>
 <template>
-    <ContractDetail />
-    <TransactionContract />
+  <ContractDetail :chain="chain"/>
+  <TransactionContract :chain="chain"/>
 </template>
