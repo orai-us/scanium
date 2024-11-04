7a2a48dd7de905ab8e869f8da767987fb64db70b
NxHuan <huan.nx@orai.io>
2024-11-04 16:59:46 +0700

    update: complete tx account

--- src/components/account/TransactionAccountIndexs.vue
+++ src/components/account/TransactionAccountIndexs.vue
@@ -10,98 +10,6 @@ import TransactionTable from "../TransactionTable.vue";
 const props = defineProps(['txs', 'chain', 'address'])
 const format = useFormatter();
 
-// const transactions = ref([
-//   {
-//     "__typename": "Transaction",
-//     "id": "D1CECE1DB35AE55C81F28E99B2B36BCAC9BA181DB6E3B1CEE0DAA5413C0ADE0E",
-//     "blockNumber": "36000000",
-//     "gasUsed": "65995",
-//     "timestamp": "1728833537678",
-//     "sender": "orai1za07jsxl9hhwmu3kth3u93sq8jstzd5mvrpvqk",
-//     "messages": {
-//       "__typename": "TransactionMessagesConnection",
-//       "nodes": [
-//         {
-//           "__typename": "TransactionMessage",
-//           "type": "/cosmwasm.wasm.v1.MsgExecuteContract",
-//           "subType": "propose"
-//         }
-//       ]
-//     }
-//   },
-//   {
-//     "__typename": "Transaction",
-//     "id": "D639DCCA140E9AE65B2221D77322C346EA5E04D3BBDD409858B91BBA67C16B73",
-//     "blockNumber": "36000001",
-//     "gasUsed": "65995",
-//     "timestamp": "1728833538368",
-//     "sender": "orai18rr2d53nl56nweanpnkg65dyj7ulua6myh4xvn",
-//     "messages": {
-//       "__typename": "TransactionMessagesConnection",
-//       "nodes": [
-//         {
-//           "__typename": "TransactionMessage",
-//           "type": "/cosmwasm.wasm.v1.MsgExecuteContract",
-//           "subType": "propose"
-//         }
-//       ]
-//     }
-//   },
-//   {
-//     "__typename": "Transaction",
-//     "id": "A4888C7F496D5F525C0B24D74A345973A2317FED8512986E2CA48A23FFADD0BF",
-//     "blockNumber": "36000007",
-//     "gasUsed": "66085",
-//     "timestamp": "1728833542913",
-//     "sender": "orai18rr2d53nl56nweanpnkg65dyj7ulua6myh4xvn",
-//     "messages": {
-//       "__typename": "TransactionMessagesConnection",
-//       "nodes": [
-//         {
-//           "__typename": "TransactionMessage",
-//           "type": "/cosmwasm.wasm.v1.MsgExecuteContract",
-//           "subType": "vote"
-//         }
-//       ]
-//     }
-//   },
-//   {
-//     "__typename": "Transaction",
-//     "id": "6AB1DEAE8C93B8D2E973690B3334E7C2E08125FBE0CB2E94996B8261C088920F",
-//     "blockNumber": "36000009",
-//     "gasUsed": "66085",
-//     "timestamp": "1728833544641",
-//     "sender": "orai1gu0vpuv4vkyhzllqqnxmkkehdqvtmltu9gkrpe",
-//     "messages": {
-//       "__typename": "TransactionMessagesConnection",
-//       "nodes": [
-//         {
-//           "__typename": "TransactionMessage",
-//           "type": "/cosmwasm.wasm.v1.MsgExecuteContract",
-//           "subType": "vote"
-//         }
-//       ]
-//     }
-//   },
-//   {
-//     "__typename": "Transaction",
-//     "id": "F2697067DB3615B3A9835738D7062B4885CAFF98E6BC8AFF3FF958DE05117003",
-//     "blockNumber": "36000015",
-//     "gasUsed": "66058",
-//     "timestamp": "1728833549056",
-//     "sender": "orai1hf2x7qu24qp5efh76ndcuc66e2qms2gk96ssqu",
-//     "messages": {
-//       "__typename": "TransactionMessagesConnection",
-//       "nodes": [
-//         {
-//           "__typename": "TransactionMessage",
-//           "type": "/cosmwasm.wasm.v1.MsgExecuteContract",
-//           "subType": "propose"
-//         }
-//       ]
-//     }
-//   }
-// ]);
 const transactions = ref()
 const totalCount = ref();
 const pagination = reactive({
@@ -119,6 +27,7 @@ const query = gql`
             timestamp
             sender
             fee
+            code
             messages {
               nodes {
                 type
@@ -142,7 +51,7 @@ const variables = computed(() => {
         },
       },
     },
-    orderBy: "BLOCK_NUMBER_ASC",
+    orderBy: "TIMESTAMP_ASC",
     first: pagination.limit,
     offset: pagination.offset
   }
@@ -165,6 +74,6 @@ function handlePagination(page: number) {
 </script>
 
 <template>
-  <TransactionTable :transaction="transactions" :chain="chain" :txTotal="totalCount" :pagination="pagination"
+  <TransactionTable :transactions="transactions" :chain="chain" :txTotal="totalCount" :pagination="pagination"
     :handlePagination="handlePagination" />
 </template>
