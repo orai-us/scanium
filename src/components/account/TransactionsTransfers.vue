fd0c5e1294e8c4a52972f8378a67bc604ae4d1a5
NxHuan <huan.nx@orai.io>
2024-11-06 18:27:21 +0700

    update: add field token for token transfer

--- src/components/account/TransactionsTransfers.vue
+++ src/components/account/TransactionsTransfers.vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
 import { TxsHistory } from '@/libs/client';
 import { useFormatter } from '@/stores';
-import { onMounted, reactive, ref, watch } from 'vue';
+import { onMounted, reactive, ref, toRaw, watch, watchEffect } from 'vue';
 import { getHistoryTxs } from '@/service/transactionsService';
 import TransactionTable from '../TransactionTable.vue';
+import { shortenTxHash } from '@/utils';
 
 const props = defineProps(['address', 'chain']);
-const txs = ref({} as TxsHistory[]);
+const txs = ref([] as Array<any>);
 const format = useFormatter();
 const txTotal = ref(0)
 
@@ -29,11 +30,11 @@ async function fetchTransaction() {
     console.log({ response })
     if (!!response) {
       txs.value = response.data.map((item: any) => ({
-        id: item.txhash,
-        code: item.status,
-        message: "",
+        ...item,
+        result: item.status === 0 ? "Success" : "Failed",
         blockNumber: item.height,
-        fee: [{amount: Number(item.fee[0]), denom: item.tokenInfos[0].abbr}],
+        fee: `${Number(item.fee[0]) / 1e6} ORAI`,
+        token: item.tokenInfos[0].denom.toUpperCase(),
         timestamp: item.timestamp * 1000
       }));
       txTotal.value = response.totalRecord;
@@ -46,6 +47,9 @@ async function fetchTransaction() {
 watch(pagination, () => {
   fetchTransaction()
 })
+watchEffect(()=>{
+  console.log({ data: toRaw(txs.value) })
+})
 
 function handlePagination(page: number) {
   pagination.offset = (page - 1) * pagination.limit
@@ -54,7 +58,52 @@ function handlePagination(page: number) {
 
 <template>
   <div>
-    <TransactionTable :transactions="txs" :chain="chain" :txTotal="txTotal" :pagination="pagination" :handlePagination="handlePagination" />
+    <div class="overflow-x-auto">
+      <table class="table w-full text-sm" v-if="txs?.length > 0">
+        <thead>
+          <tr>
+            <th>Tx Hash</th>
+            <th>Result</th>
+            <th>Height</th>
+            <th>Fee</th>
+            <th>Token</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody class="text-sm">
+          <tr v-for="(v, index) in txs" :key="index">
+            <td class="truncate py-3" style="max-width: 200px">
+              <RouterLink :to="`/${chain}/tx/${v.txhash}`" class="text-primary dark:text-link">
+                {{ shortenTxHash(v.txhash) }}
+              </RouterLink>
+            </td>
+            <td class="text-sm py-3" :class="`${v.result === 'Success' ? 'text-[#39DD47]' : 'text-error'
+              }`">
+              {{ v.result }}
+            </td>
+            <td class="text-sm py-3">
+              <RouterLink :to="`/${chain}/block/${v.height}`" class="text-primary dark:text-link">{{
+                v.height }}
+              </RouterLink>
+            </td>
+            <td class="py-3">
+              <span class="text-xs">{{ v.fee || "-" }}</span>
+            </td>
+            <td class="py-3">
+              <span class="text-xs">{{ v.token }}</span>
+            </td>
+            <td>{{ v.timestamp || "-" }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-else class="flex items-center justify-center w-full h-full">
+        <td>No Transactions</td>
+      </div>
+    </div>
+
+    <div class="mt-4 text-center" v-if="txTotal">
+      <Pagination :totalItems="txTotal" :limit="pagination.limit" :onPagination="handlePagination" />
+    </div>
   </div>
 </template>
 
