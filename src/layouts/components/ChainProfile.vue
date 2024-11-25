2200747d2149d81979e06e9e6b45cdc1e4266258
NxHuan <huan.nx@orai.io>
2024-11-25 16:47:38 +0700

    update: format file

--- src/layouts/components/ChainProfile.vue
+++ src/layouts/components/ChainProfile.vue
@@ -44,18 +44,27 @@ onMounted(() => {
   <div class="dropdown">
     <label tabindex="0" class="flex items-center">
       <div class="p-1 relative mr-3 cursor-pointer">
-        <img v-lazy="chainStore.logo" class="w-9 h-9 rounded-full bg-white border border-white" />
-        <div class="w-2 h-2 rounded-full absolute right-0 bottom-0 shadow" :class="{
-          'bg-success': baseStore.connected,
-          'bg-error': !baseStore.connected,
-        }"></div>
+        <img
+          v-lazy="chainStore.logo"
+          class="w-9 h-9 rounded-full bg-white border border-white"
+        />
+        <div
+          class="w-2 h-2 rounded-full absolute right-0 bottom-0 shadow"
+          :class="{
+            'bg-success': baseStore.connected,
+            'bg-error': !baseStore.connected,
+          }"
+        ></div>
       </div>
       <div class="flex-1 w-0">
-        <div :key="baseStore.latest?.block?.header?.height ||
-          chainStore.chainName ||
-          ''
+        <div
+          :key="
+            baseStore.latest?.block?.header?.height ||
+            chainStore.chainName ||
+            ''
           "
-          class="capitalize whitespace-nowrap text-base font-semibold text-gray-600 dark:text-gray-200 hidden md:!block">
+          class="capitalize whitespace-nowrap text-base font-semibold text-gray-600 dark:text-gray-200 hidden md:!block"
+        >
           {{
             baseStore.latest?.block?.header?.height
               ? `#${baseStore.latest.block.header.height}`
@@ -63,14 +72,19 @@ onMounted(() => {
           }}
           <span class="text-error">{{
             baseStore.connected ? '' : 'disconnected'
-            }}</span>
+          }}</span>
         </div>
-        <div class="text-xs text-gray-500 dark:text-gray-400 whitespace-nowrap hidden md:!block">
+        <div
+          class="text-xs text-gray-500 dark:text-gray-400 whitespace-nowrap hidden md:!block"
+        >
           {{ chainStore.connErr || chainStore.endpoint.address }}
         </div>
       </div>
     </label>
-    <div tabindex="0" class="dropdown-content flex-row left-6 w-80 menu shadow bg-base-200 rounded-box overflow-auto">
+    <div
+      tabindex="0"
+      class="dropdown-content flex-row left-6 w-80 menu shadow bg-base-200 rounded-box overflow-auto"
+    >
       <!-- rest -->
       <div class="px-4 py-2 text-sm text-gray-400">Information</div>
       <div class="w-full">
@@ -93,28 +107,41 @@ onMounted(() => {
       </div>
 
       <!-- rest -->
-      <div class="px-4 py-2 text-sm text-gray-400 text-bold" v-if="chainStore.current?.endpoints?.rpc">
+      <div
+        class="px-4 py-2 text-sm text-gray-400 text-bold"
+        v-if="chainStore.current?.endpoints?.rpc"
+      >
         RPC Endpoint
       </div>
-      <div v-for="(item, index) in chainStore.current?.endpoints?.rpc"
-        class="px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-[#384059] cursor-pointer" :key="index"
-        @click="changeEndpoint(item)">
+      <div
+        v-for="(item, index) in chainStore.current?.endpoints?.rpc"
+        class="px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-[#384059] cursor-pointer"
+        :key="index"
+        @click="changeEndpoint(item)"
+      >
         <div class="flex flex-col">
           <div class="flex items-center justify-between w-full">
             <div class="text-gray-500 dark:text-gray-200 capitalize">
               {{ item.provider }}
             </div>
-            <span v-if="item.address === chainStore.endpoint?.address"
-              class="bg-yes inline-block h-2 w-2 rounded-full" />
+            <span
+              v-if="item.address === chainStore.endpoint?.address"
+              class="bg-yes inline-block h-2 w-2 rounded-full"
+            />
           </div>
           <div class="text-gray-400 text-xs">
             {{ item.address }}
           </div>
         </div>
       </div>
-      <input v-model="rpcModel" class="input w-full !input-bordered input-sm" @keydown.enter.prevent="
-        changeEndpoint({ address: rpcModel, provider: 'custom' })
-        " :placeholder="'custom: https://rpc.' + chainStore.chainName + '.io'" />
+      <input
+        v-model="rpcModel"
+        class="input w-full !input-bordered input-sm"
+        @keydown.enter.prevent="
+          changeEndpoint({ address: rpcModel, provider: 'custom' })
+        "
+        :placeholder="'custom: https://rpc.' + chainStore.chainName + '.io'"
+      />
 
       <!-- bottom-->
       <div class="px-4 py-2">&nbsp;</div>
