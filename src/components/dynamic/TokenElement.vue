1f524eb66aaa1b7ed45f561d8621eb8f7da6a8ac
NxHuan <huan.nx@orai.io>
2024-12-10 17:05:48 +0700

    update: fix amount in message tx detail

--- src/components/dynamic/TokenElement.vue
+++ src/components/dynamic/TokenElement.vue
@@ -1,21 +1,28 @@
 <script lang="ts" setup>
 import axios from 'axios';
-import { ref, watchEffect } from 'vue';
+import { ref, toRaw, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 
 const props = defineProps(['value']);
 
 const route = useRoute();
 const nameToken = ref("");
+let exponent = ref(0);
 
 watchEffect(() => {
+  console.log({amount: toRaw(props.value)})
   async function fetchName() {
     const denom = props.value["denom"];
     if (denom?.includes("/")) {
       const responseRegistry = await axios(`https://registry.ping.pub/${route.params?.chain?.toString().toLowerCase()}/assetlist.json`);
       const assets = responseRegistry.data.assets as Array<any>;
-      const name = assets.find((asset: any) => asset.base === denom || (Array.isArray(asset.traces) && asset.traces[0]?.chain?.path === denom))?.display;
-      nameToken.value = name;
+      const asset = assets.find((asset: any) => asset.base === denom || (Array.isArray(asset.traces) && asset.traces[0]?.chain?.path === denom));
+      if (asset) {
+        const name = asset.display;
+        const denomUnits = asset.denom_units;
+        exponent.value = denomUnits.find((denomUnit: any) => denomUnit.denom === name)?.exponent;
+        nameToken.value = name;
+      }
     } else {
       nameToken.value = denom;
     }
@@ -26,6 +33,6 @@ watchEffect(() => {
 </script>
 <template>
   <div class="overflow-auto">
-      {{ Number(value["amount"]).toLocaleString("en-US", {}) }} {{ nameToken }}
+    {{ (Number(value["amount"]) / Math.pow(10, exponent)).toLocaleString("en-US", {}) }} {{ nameToken }}
   </div>
 </template>
