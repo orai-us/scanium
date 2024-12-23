0f236195014789fa5ac6f6f2c378e45e04214a2c
NinhXuanHuan <137025478+ninhxuanhuan@users.noreply.github.com>
2024-12-23 17:39:13 +0700

    update: add tx receiver and lable IN-OUT for list account's tx (#38)
    * update: add status for token transfer
    
    * update: add tx reciver and status tx
    
    * update: change logic in, out transaction

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,36 @@ export const shortenTxHash = (txHash: string) => {
   const suffixHash = txHash.slice(-6);
   return `${prefixHash} . . . ${suffixHash}`;
 };
+
+export const labelInOutTxs = (txs: Array<any>, addAccount: string) => {
+  return txs.map((item) => {
+    let state = null;
+    const nodes = item.messages?.nodes;
+    if (Array.isArray(nodes)) {
+      if (
+        nodes[0]?.type === '/cosmos.bank.v1beta1.MsgSend' ||
+        nodes[0]?.subType === 'send'
+      ) {
+        if (item.sender === addAccount) state = 'OUT';
+        else state = 'IN';
+      }
+      if (nodes[0]?.type?.includes('ibc')) {
+        const tokenTransfers = item.tokenTransfers?.nodes;
+        if (Array.isArray(tokenTransfers)) {
+          let numberTxOut = 0, numberTxIn = 0;
+          numberTxOut = tokenTransfers.filter((item) => item.from === addAccount).length;
+          numberTxIn = tokenTransfers.filter((item) => item.to === addAccount).length;
+          if (numberTxOut > numberTxIn)
+            state = 'OUT';
+          else if(numberTxOut < numberTxIn)
+            state = 'IN';
+        }
+      }
+    }
+
+    return {
+      ...item,
+      state,
+    };
+  });
+};
