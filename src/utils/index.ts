ff3dcfed2052432f8677ef83a616a6f7f3eabdd0
NxHuan <huan.nx@orai.io>
2024-12-23 19:58:12 +0700

    update: fix merge error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -35,12 +35,28 @@ export const labelInOutTxs = (txs: Array<any>, addAccount: string) => {
       state,
     };
   });
+}
+
+export const shortenDenom = (denom: string) => {
+  if (denom.length <= 10) return denom;
+  const prefixHash = denom.slice(0, 10);
+  const suffixHash = denom.slice(-10);
+  return `${prefixHash} . . . ${suffixHash}`;
+};
+
+export const convertNewAssetToToken = (
+  newAssets: Array<{ coingecko_id: string; display: string }>
+) => {
+  const token: any = {};
+  for (let asset of newAssets) {
+    token[asset.coingecko_id] = asset.display;
+  }
+  return token;
 };
 
 export function formatNumber(number: string | number) {
   const result = Number(number);
   if (isNaN(result)) return "-";
-  if (number === 0) return 0;
   if (Math.abs(result) < 1) {
     return result.toLocaleString("en-US", {
       minimumFractionDigits: 5,
