3f5983c73c6cf2a322c404f5967d15b3cc9baad1
NxHuan <huan.nx@orai.io>
2024-12-29 22:10:24 +0700

    update: add comment for display rack token

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -30,7 +30,7 @@ export const NEW_ASSETS = [
   {
     name: 'factory/orai1wuvhex9xqs3r539mvc6mtm7n20fcj3qr2m0y9khx6n5vtlngfzes3k0rq9/D7yP4ycfsRWUGYionGpi64sLF2ddZ2JXxuRAti2M7uck',
     base: 'factory/orai1wuvhex9xqs3r539mvc6mtm7n20fcj3qr2m0y9khx6n5vtlngfzes3k0rq9/D7yP4ycfsRWUGYionGpi64sLF2ddZ2JXxuRAti2M7uck',
-    display: 'racks',
+    display: 'racks', // Not used to display on assets and messages
     symbol: 'RACKS',
     logo_URIs: {
       svg: 'https://ipfs.io/ipfs/QmVRPsBSHpamDzcDzVL9wsbB9gr4frtNrrFF7g44Xa9FuS',
