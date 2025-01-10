ba585ca772b673687a9169e1b640d50e5b892eea
NinhXuanHuan <137025478+ninhxuanhuan@users.noreply.github.com>
2025-01-10 11:17:12 +0700

    update: optimal get txs account (#64)
    * update: optimal list tx for account
    
    * update: clear import txs account

--- src/service/transactionsService.ts
+++ src/service/transactionsService.ts
@@ -1,18 +1,40 @@
 import { api, METHODS } from './api';
 
 const baseUrlOraidex = 'https://tx-history-backend.oraidex.io';
+const baseUrlOptimalQueriesScanium = 'https://api-optimal-queries.scanium.io';
 
 export interface ParamsGetHistoryTxs {
   addrByNetworks: string;
   limit?: number;
   offset?: number;
 }
-const urlTxsHistory = "/v1/txs-history/"
 
+export interface ParamsGetTxsAccount {
+  page: number;
+  limit: number,
+  count: boolean
+}
+
+const urlTxsHistory = "/v1/txs-history/"
 export const getHistoryTxs = async (params: ParamsGetHistoryTxs) => {
   const config = {
     baseURL: baseUrlOraidex,
     url: urlTxsHistory,
+    method: METHODS.GET,
+    params,
+  };
+  const res = await api.request(config);
+  return res?.data;
+};
+
+const urlTxsByAccount = "/v1/transaction/by-account"
+export const getTxsAccount = async (
+  addressAcc: string,
+  params: ParamsGetTxsAccount
+) => {
+  const config = {
+    baseURL: baseUrlOptimalQueriesScanium,
+    url: `${urlTxsByAccount}/${addressAcc}`,
     method: METHODS.GET,
     params,
   };
