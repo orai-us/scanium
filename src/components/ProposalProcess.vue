a69c8fbd425291ac46d85e07f483e2b2f05178d2
NxHuan <huan.nx@orai.io>
2025-01-22 15:34:07 +0700

    update: fix Percent in list voting

--- src/components/ProposalProcess.vue
+++ src/components/ProposalProcess.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup>
 import { useFormatter } from '@/stores';
-import { computed } from '@vue/reactivity';
-import type { TallyResult } from 'cosmjs-types/cosmos/gov/v1beta1/gov';
-import type { PropType } from 'vue';
+import { computed, ref } from '@vue/reactivity';
+import { watchEffect, type PropType } from 'vue';
 
 const props = defineProps({
-  tally: { type: Object as PropType<TallyResult> },
+  tally: { type: Object as PropType<any> },
   pool: {
     type: Object as PropType<{
       notBondedTokens: string;
@@ -16,18 +15,19 @@ const props = defineProps({
 
 const total = computed(() => props.pool?.bondedTokens);
 const format = useFormatter();
-const yes = computed(() =>
-  format.calculatePercent(props.tally?.yes, total.value)
-);
-const no = computed(() =>
-  format.calculatePercent(props.tally?.no, total.value)
-);
-const abstain = computed(() =>
-  format.calculatePercent(props.tally?.abstain, total.value)
-);
-const veto = computed(() =>
-  format.calculatePercent(props.tally?.noWithVeto, total.value)
-);
+
+const yes = ref("0");
+const no = ref("0");
+const abstain = ref("0");
+const veto = ref("0");
+
+watchEffect(() => {
+  yes.value = format.calculatePercent(props.tally?.yesCount || props.tally?.yes, total.value);
+  no.value = format.calculatePercent(props.tally?.noCount || props.tally?.no, total.value);
+  abstain.value = format.calculatePercent(props.tally?.abstainCount || props.tally?.abstain, total.value);
+  veto.value = format.calculatePercent(props.tally?.noWithVetoCount || props.tally?.noWithVetoCount, total.value);
+})
+
 </script>
 
 <template>
@@ -35,28 +35,28 @@ const veto = computed(() =>
     <div
       class="h-6 bg-yes flex items-center pl-2 text-white overflow-hidden"
       :style="`width: ${yes}`"
-      :title="yes"
+      :title="'yes'"
     >
       {{ yes }}
     </div>
     <div
       class="h-6 bg-no flex items-center text-white overflow-hidden"
       :style="`width: ${no}`"
-      :title="no"
+      :title="'no'"
     >
       {{ no }}
     </div>
     <div
       class="h-6 bg-[#B71C1C] flex items-center text-white overflow-hidden"
       :style="`width: ${veto};`"
-      :title="veto"
+      :title="'veto'"
     >
       {{ veto }}
     </div>
     <div
       class="h-6 bg-secondary flex items-center text-white overflow-hidden"
       :style="`width: ${abstain}`"
-      :title="abstain"
+      :title="'abstain'"
     >
       {{ abstain }}
     </div>
