717fb3fc3c99c1beeced1b36c0b2399638886f84
NinhXuanHuan <137025478+ninhxuanhuan@users.noreply.github.com>
2025-02-13 09:20:10 +0700

    update: fix round time (#86)

--- src/stores/useFormatter.ts
+++ src/stores/useFormatter.ts
@@ -25,9 +25,22 @@ import type { Coin } from '@cosmjs/stargate';
 import type { Timestamp } from 'cosmjs-types/google/protobuf/timestamp';
 import { fromTimestamp } from 'cosmjs-types/helpers';
 
+const thresholds = [
+  { l: 's', r: 1 },
+  { l: 'm', r: 1 },
+  { l: 'mm', r: 59, d: 'minute' },
+  { l: 'h', r: 1 },
+  { l: 'hh', r: 23, d: 'hour' },
+  { l: 'd', r: 1 },
+  { l: 'dd', r: 29, d: 'day' },
+  { l: 'M', r: 1 },
+  { l: 'MM', r: 11, d: 'month' },
+  { l: 'y', r: 1 },
+  { l: 'yy', d: 'year' },
+];
 dayjs.extend(localeData);
 dayjs.extend(duration);
-dayjs.extend(relativeTime);
+dayjs.extend(relativeTime, { thresholds });
 dayjs.extend(updateLocale);
 dayjs.extend(utc);
 dayjs.updateLocale('en', {
