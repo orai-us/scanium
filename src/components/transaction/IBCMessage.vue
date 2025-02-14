6b0db9210bb83eb1bef42e20533e4a5809781421
NxHuan <huan.nx@orai.io>
2025-02-14 15:27:17 +0700

    update: add list ibc token

--- src/components/transaction/IBCMessage.vue
+++ src/components/transaction/IBCMessage.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import { ref, toRaw, watchEffect } from 'vue';
 import { select } from '../dynamic';
+import { getListAsset } from '@/service/assetsService';
+import { useRoute } from 'vue-router';
 
 const props = defineProps(['value', 'type']);
 const data = ref({} as any);
+const route = useRoute();
 
 function formatNumber(number: string | number, options = {}) {
   const result = Number(number);
@@ -11,7 +14,40 @@ function formatNumber(number: string | number, options = {}) {
   return result.toLocaleString("en-US", options);
 }
 
-watchEffect(() => {
+const TOKEN_IBCS = [
+  {
+    base: "oraib0x55d398326f99059fF775485246999027B3197955",
+    denom: "oraib0x55d398326f99059fF775485246999027B3197955",
+    display: "USDT BSC",
+    exponent: 18
+  },
+  {
+    base: "oraib0xA325Ad6D9c92B55A3Fc5aD7e412B1518F96441C0",
+    denom: "oraib0xA325Ad6D9c92B55A3Fc5aD7e412B1518F96441C0",
+    display: "ORAI BSC",
+    exponent: 18
+  },
+  {
+    base: "oraib0x7e2A35C746F2f7C240B664F1Da4DD100141AE71F",
+    denom: "oraib0x7e2A35C746F2f7C240B664F1Da4DD100141AE71F",
+    display: "AIRI BSC",
+    exponent: 18
+  },
+  {
+    base: "oraib0x257a8d1E03D17B8535a182301f15290F11674b53",
+    denom: "oraib0x257a8d1E03D17B8535a182301f15290F11674b53",
+    display: "KWT BSC",
+    exponent: 18
+  },
+  {
+    base: "eth-mainnet0X4C11249814F11B9346808179CF06E71AC328C1B5",
+    denom: "eth-mainnet0X4C11249814F11B9346808179CF06E71AC328C1B5",
+    display: "ORAI ERC20",
+    exponent: 18
+  },
+]
+
+watchEffect(async () => {
   const decoder = new TextDecoder('utf-8');
   const value = props.value;
   switch (props.type) {
@@ -26,13 +62,29 @@ watchEffect(() => {
       const packet = props.value?.packet;
       const dataPacket = packet?.data;
       const parseData = JSON.parse(decoder.decode(dataPacket));
-      const amount = formatNumber(parseData?.amount);
+      let amount = parseData?.amount;
       const denom = parseData?.denom;
+      const assets = await getListAsset(route.params?.chain?.toString());
+      const asset = [...assets, ...TOKEN_IBCS].find((asset: any) => asset.base === denom || (Array.isArray(asset.traces) && asset.traces[0]?.chain?.path === denom));
+      let exponent = 0;
+      if (asset) {
+        const name = asset.display;
+        if (asset.exponent) {
+          exponent = Number(asset.exponent);
+        } else {
+          const denomUnits = asset.denom_units;
+          if (Array.isArray(denomUnits)) {
+            exponent = denomUnits.find((denomUnit: any) => denomUnit.denom === name)?.exponent;
+          }
+        }
+      }
+      amount = formatNumber(Number(amount) / 10 ** exponent);
+      const originDenom =  asset?.display || denom;
       data.value = {
         sequence: formatNumber(packet?.sequence),
-        amount: { amount: parseData?.amount, denom },
+        amount: `${amount} ${originDenom}`,
         originAmount: amount,
-        originDenom: denom,
+        originDenom: originDenom,
         receiver: parseData?.receiver,
         sender: parseData?.sender,
         sourcePort: packet?.sourcePort,
