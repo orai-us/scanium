1a8f802f4660facf7942ecd8189852330242b34d
NxHuan <huan.nx@orai.io>
2025-02-25 17:36:22 +0700

    update: get tx on contract

--- src/components/contracts/TransactionContractIndexs.vue
+++ src/components/contracts/TransactionContractIndexs.vue
@@ -1,76 +1,124 @@
 <script lang="ts" setup>
-import { computed, ref, watchEffect } from 'vue';
+import { computed, onMounted, ref, watchEffect } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
 import { reactive } from "vue";
 import TransactionTable from "../TransactionTable.vue";
+import { countTxsOnContract, getTxsOnContract, ParamsGetTx } from '@/service/transactionsService';
+import { useRoute, useRouter } from 'vue-router';
+import { labelInOutTxs } from '@/utils';
 
-const props = defineProps(['txs', 'chain', 'address'])
+const props = defineProps(['txs', 'chain', 'address']);
+const route = useRoute();
+const router = useRouter();
 
 // Transaction in contract
 const transactions = ref();
 const txTotal = ref();
-const pagination = reactive({
-  limit: 10,
-  offset: 0
-})
+
+const pagination = computed(() => {
+  const page = route.query.page ? Number(route.query.page) : 1;
+  return {
+    page,
+    limit: 10,
+  };
+});
+
+async function fetchTxs(pagination: ParamsGetTx) {
+  try {
+    const res = await getTxsOnContract(props.address, pagination);
+    if (res) {
+      transactions.value = res.data;
+    }
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+
+async function fetchCountTxs() {
+  try {
+    const res = await countTxsOnContract(props.address);
+    if (res) {
+      txTotal.value = res.data;
+    }
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+onMounted(() => {
+  fetchCountTxs();
+});
+
+watchEffect(() => {
+  fetchTxs(pagination.value);
+});
+
+const txHashes = computed(() => {
+  return transactions.value?.map((tx: any) => tx.id);
+});
 
 const query = gql`
-      query GetTransactions($filter: TransactionFilter!, $orderBy: [TransactionsOrderBy!], $first: Int!, $offset: Int!) {
-        transactions(filter: $filter, orderBy: $orderBy, first: $first, offset:$offset) {
+      query GetTransactions($filter: TransactionFilter!) {
+        transactions(filter: $filter) {
           results: nodes {
             id
-            blockNumber
-            gasUsed
-            timestamp
-            sender
-            fee
-            code
             messages {
               nodes {
                 type
                 subType
               }
             }
+            tokenTransfers {
+              nodes {
+                denom
+                from
+                to
+                amount
+                type
+              }
+            }
           }
-          totalCount
         }
       }
-`;
+    `;
 
 const variables = computed(() => {
   return {
     filter: {
-      messages: {
-        some: {
-          contract: {
-            equalTo: props.address,
-          },
-        },
-      },
+      id: { in: txHashes.value }
     },
-    orderBy: "BLOCK_NUMBER_DESC",
-    first: pagination.limit,
-    offset: pagination.offset
-  }
-})
+  };
+});
 
-const { result, refetch } = useQuery(query, variables);
+const { result } = useQuery(query, variables);
 
-watchEffect(() => {
-  if (result.value) {
-    transactions.value = result.value.transactions.results;
-    txTotal.value = result.value.transactions.totalCount;
-  }
-})
+const txsMerge = computed(() => {
+  const txsOptimal = transactions.value;
+  const txsIndexer = result.value?.transactions?.results;
 
-function handlePagination(page: number) {
-  pagination.offset = (page - 1) * pagination.limit;
-  refetch()
+  const data = txsOptimal?.map((txOptimal: any) => {
+    const searchTx = txsIndexer?.find((txIndexer: any) => txIndexer.id === txOptimal.id);
+    const messages = searchTx?.messages;
+    const tokenTransfers = searchTx?.tokenTransfers;
+
+    return {
+      ...txOptimal,
+      messages,
+      tokenTransfers
+    };
+  });
+
+  return labelInOutTxs(data, props.address);
+});
+
+async function handlePagination(page: number) {
+  router.push({ path: `/${props.chain}/account/${props.address}`, query: { ...route.query, page } });
 }
 
 </script>
 <template>
-  <TransactionTable :transactions="transactions" :chain="chain" :txTotal="txTotal" :limit="pagination.limit"
+  <TransactionTable :transactions="txsMerge" :chain="chain" :txTotal="txTotal" :limit="pagination.limit"
     :handlePagination="handlePagination" />
 </template>
