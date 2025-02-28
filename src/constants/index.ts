43f6c1b9b6ea606864610177fdb2282c17fecdd1
NxHuan <huan.nx@orai.io>
2025-02-28 14:11:15 +0700

    update: labeing for addres

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,5 +14,11 @@ export const LABELING_ADDRESS: any = {
   orai1etznkvedku7k0t9unaudjkfm4v6xyx26y3fyfw: 'Oraichain: Ecosystem (Deprecated02)',
   orai1603j3e4juddh7cuhfquxspl0p0nsun04f0xaju: 'Oraichain: Utilities',
   orai1mj4wv6lxdd4uldnl4u2hkeq37g9hfr9krf6tvh: 'Oraichain: Ecosystem (Deprecated01)',
-  orai14regzsh67mq5gyrqf7sge45ak46pkr0cxwymh2: 'Oraichain: Bridge'
+  orai14regzsh67mq5gyrqf7sge45ak46pkr0cxwymh2: 'Oraichain: Bridge',
+  orai1ym6qytsu7skv2flw89y0mkey4gn7wl9q4y6r5p: 'Oraichain: Solana Bridge Hot Wallet 1',
+  orai159l8l9c5ckhqpuwdfgs9p4v599nqt3cjlfahalmtrhfuncnec2ms5mz60e: 'Oraichain: Ton Bridge Adapter',
+  orai195269awwnt5m6c843q6w7hp8rt0k7syfu9de4h0wz384slshuzps8y7ccm: 'Oraichain: IBC Wasm Bridge',
+  orai1g90x3z2kss99wvmpkenjdelmpw4hf9l3yt420gpgqvpuz8lt79uq24arlv: 'Oraichain: BTC Bridge Application',
+  orai1qvk2fwr3g5uaj86cjvncr2u5r4pfm92dj4lkz50sgrax45hmeurqy9tuyu: 'Oraichain: BTC Bridge Light Client',
+  orai10s0c75gw5y5eftms5ncfknw6lzmx0dyhedn75uz793m8zwz4g8zq4d9x9a: 'Oraidex: AMM v3'
 }
