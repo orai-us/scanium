bb80f0f6b07974ed763b4c8bcd7a6a56a4088ae4
NxHuan <huan.nx@orai.io>
2025-02-28 11:59:45 +0700

    update: add pagination for tx contract

--- src/components/contracts/TransactionContractIndexs.vue
+++ src/components/contracts/TransactionContractIndexs.vue
@@ -13,13 +13,14 @@ const route = useRoute();
 const router = useRouter();
 
 // Transaction in contract
-const transactions = ref();
+const transactions = ref([]);
 const txTotal = ref();
+const currentPage = ref(1);
 
 const pagination = computed(() => {
-  const page = route.query.page ? Number(route.query.page) : 1;
+  // const page = route.query.page ? Number(route.query.page) : 1;
   return {
-    page,
+    page: currentPage.value,
     limit: 10,
   };
 });
@@ -47,9 +48,9 @@ async function fetchCountTxs() {
   }
 }
 
-onMounted(() => {
-  fetchCountTxs();
-});
+// onMounted(() => {
+//   fetchCountTxs();
+// });
 
 watchEffect(() => {
   fetchTxs(pagination.value);
@@ -117,8 +118,23 @@ async function handlePagination(page: number) {
   router.push({ path: `/${props.chain}/account/${props.address}`, query: { ...route.query, page } });
 }
 
+function handlePrevious() {
+  let page = currentPage.value;
+  if (page == 1) return;
+  currentPage.value = page - 1;
+}
+
+function handleNext() {
+  let page = currentPage.value;
+  currentPage.value = page + 1;
+}
+
 </script>
 <template>
   <TransactionTable :transactions="txsMerge" :chain="chain" :txTotal="txTotal" :limit="pagination.limit"
     :handlePagination="handlePagination" />
+  <div class="flex items-center justify-center w-full mt-5">
+    <button class="flex justify-center items-center px-4 py-2 bg-base rounded-s-lg w-20 border-r-2 border-[#383B40]" v-on:click="handlePrevious">Previous</button>
+    <button class="flex justify-center items-center px-4 py-2 bg-base rounded-e-lg w-20" v-on:click="handleNext">Next</button>
+  </div>
 </template>
