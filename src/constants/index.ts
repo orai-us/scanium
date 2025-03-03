45bb33728eb3df7cfa4e031551669561a33e8a2f
NinhXuanHuan <137025478+ninhxuanhuan@users.noreply.github.com>
2025-03-03 15:29:51 +0700

    update label for address (#104)
    * update: add labeling for address
    
    * update:match label

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,24 @@
 export const CHAIN_INDEXS = ['Oraichain'];
+
+export const LABELING_ADDRESS: any = {
+  orai1fwq0ct8ww0hm574vsdrhj7p5h0rd0vcz2hxsd0hfufwdlw4u78ksp4wj4y: 'Oraichain: DAO',
+  orai1grzpefjywa2w0vcs0d67t78h4v7pdpzp2yzh72: 'Kucoin: Exchange02',
+  orai14vcw5qk0tdvknpa38wz46js5g7vrvut8lk0lk6: 'Oraichain: Genesis node03',
+  orai1z6ma39shfskf92l8hx375a0melpdm9smtmku28: 'Oraichain: Testing',
+  orai1mxqeldsxg60t2y6gngpdm5jf3k96dnjuanyv3w: 'Oraichain: Genesis node01',
+  orai1rqq57xt5r5pnuguffcrltnvkul7n0jdx08ns0g: 'Oraichain: Genesis node02',
+  orai1dugk9jpxwkyd0gt20344vu3ltx6083zjkv5pygt8hhtv8u420k7s32wvky: 'Oraichain: Ecosystem',
+  orai1fs25usz65tsryf0f8d5cpfmqgr0xwup4kjqpa0: 'Oraichain: Foundation',
+  orai1h6d6hh3qzx0cmjxrkzz08rjc406wka22u3qc0z: 'Kucoin: Exchange01',
+  orai146tlt2sfuls7ku05msf7sa6v8saxauh2zenwlk: 'Oraichain: Deployer',
+  orai1etznkvedku7k0t9unaudjkfm4v6xyx26y3fyfw: 'Oraichain: Ecosystem (Deprecated02)',
+  orai1603j3e4juddh7cuhfquxspl0p0nsun04f0xaju: 'Oraichain: Utilities',
+  orai1mj4wv6lxdd4uldnl4u2hkeq37g9hfr9krf6tvh: 'Oraichain: Ecosystem (Deprecated01)',
+  orai14regzsh67mq5gyrqf7sge45ak46pkr0cxwymh2: 'Oraichain: Bridge',
+  orai1ym6qytsu7skv2flw89y0mkey4gn7wl9q4y6r5p: 'Oraichain: Solana Bridge Hot Wallet 1',
+  orai159l8l9c5ckhqpuwdfgs9p4v599nqt3cjlfahalmtrhfuncnec2ms5mz60e: 'Oraichain: Ton Bridge Adapter',
+  orai195269awwnt5m6c843q6w7hp8rt0k7syfu9de4h0wz384slshuzps8y7ccm: 'Oraichain: IBC Wasm Bridge',
+  orai1g90x3z2kss99wvmpkenjdelmpw4hf9l3yt420gpgqvpuz8lt79uq24arlv: 'Oraichain: BTC Bridge Application',
+  orai1qvk2fwr3g5uaj86cjvncr2u5r4pfm92dj4lkz50sgrax45hmeurqy9tuyu: 'Oraichain: BTC Bridge Light Client',
+  orai10s0c75gw5y5eftms5ncfknw6lzmx0dyhedn75uz793m8zwz4g8zq4d9x9a: 'Oraidex: AMM v3'
+}
