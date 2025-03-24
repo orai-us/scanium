103d95c4101dced02856a11a2427062427000e10
NxHuan <huan.nx@orai.io>
2025-03-24 16:10:47 +0700

    update: get time block by height

--- src/service/blocksService.ts
+++ src/service/blocksService.ts
@@ -1,57 +1,45 @@
 import { CHAIN_INDEXS } from '@/constants';
 import { useBaseStore } from '@/stores';
-import gql from 'graphql-tag';
-import { apolloClient } from '@/main';
+import { api, METHODS } from './api';
+
+const baseUrlOptimalQueriesScanium = 'https://api-optimal-queries.scanium.io';
+const urlInfoBlock = '/v1/block';
+export const getBlockByHeight = async (height?: number | string) => {
+  const config = {
+    baseURL: baseUrlOptimalQueriesScanium,
+    url: `${urlInfoBlock}/${height}`,
+    method: METHODS.GET,
+  };
+  const res = await api.request(config);
+  return res?.data;
+};
 
 class BlocksService {
   store = useBaseStore();
 
-  getBlockDetail = async (chainName: string, height?: number | string): Promise<{blocks?: any, sumAggregates?: any}> => {
+  getBlockDetail = async (
+    chainName: string,
+    height?: number | string
+  ): Promise<{ blocks?: any; sumAggregates?: any }> => {
     if (CHAIN_INDEXS.includes(chainName)) {
-      const query = gql`
-        query GetBlockDetail($filterBlock: BlockFilter!, $first: Int!, $filterTx: TransactionFilter! ) {
-          blocks(filter: $filterBlock, first: $first) {
-            results: nodes {
-              id
-              chainId
-              round
-              txCount
-              proposerAddress
-              time
-              nodeId
-            }
-          }
-          transactions(filter: $filterTx) {
-            aggregates {
-              sum {
-                gasUsed
-                gasWanted
-              }
-            }
-          }
-        }
-      `;
-
-      const variables = {
-        filterBlock: { height: { equalTo: height } },
-        first: 1,
-        filterTx: { blockNumber: { equalTo: height } },
-      };
-
-      const { data } = await apolloClient.query({ query, variables });
-      const blocks = data.blocks?.results[0];
-      const sumAggregates = data.transactions?.aggregates?.sum;
-      if (blocks) return {
-        blocks, 
-        sumAggregates
-      }
-      else return {
-        blocks: await this.store.fetchBlock(height),
-      }
+      const resBlock = await getBlockByHeight(height);
+      const blocks = resBlock?.data;
+      if (blocks)
+        return {
+          blocks,
+          sumAggregates: {
+            gasWanted: blocks.gasWanted,
+            gasUsed: blocks.gasUsed,
+          },
+        };
+      else
+        return {
+          blocks: await this.store.fetchBlock(height),
+        };
     } else {
       return {
         blocks: await this.store.fetchBlock(height),
-      }
+      };
     }
   };
 }
