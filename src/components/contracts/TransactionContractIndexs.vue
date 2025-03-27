b888f51edcc1baa0c72b5764385c7b1a8c08d71f
NinhXuanHuan <137025478+ninhxuanhuan@users.noreply.github.com>
2025-03-27 10:54:51 +0700

    update: count for tx contract (#157)

--- src/components/contracts/TransactionContractIndexs.vue
+++ src/components/contracts/TransactionContractIndexs.vue
@@ -1,12 +1,10 @@
 <script lang="ts" setup>
-import { computed, onMounted, ref, watchEffect } from 'vue';
-import { useQuery } from '@vue/apollo-composable';
-import gql from 'graphql-tag';
-import { reactive } from "vue";
+import { computed, ref, watchEffect } from 'vue';
 import TransactionTable from "../TransactionTable.vue";
-import { countTxsOnContract, getTxsOnContract, ParamsGetTx } from '@/service/transactionsService';
+import { countTxsOnContract, getListTxByTxHashes, getTxsOnContract, ParamsGetTx } from '@/service/transactionsService';
 import { useRoute, useRouter } from 'vue-router';
 import { labelInOutTxs } from '@/utils';
+import { watch } from 'vue';
 
 const props = defineProps(['txs', 'chain', 'address']);
 const route = useRoute();
@@ -14,8 +12,9 @@ const router = useRouter();
 
 // Transaction in contract
 const transactions = ref([]);
-const txTotal = ref();
+const txTotal = ref(0);
 const currentPage = ref(1);
+const txByTxHashes = ref([] as Array<any>);
 
 const pagination = computed(() => {
   // const page = route.query.page ? Number(route.query.page) : 1;
@@ -60,43 +59,35 @@ const txHashes = computed(() => {
   return transactions.value?.map((tx: any) => tx.id);
 });
 
-const query = gql`
-      query GetTransactions($filter: TransactionFilter!) {
-        transactions(filter: $filter) {
-          results: nodes {
-            id
-            messages {
-              nodes {
-                type
-                subType
-              }
-            }
-          }
-        }
-      }
-    `;
+async function fetchListTxByTxHashes (txHashes: Array<any>){
+  try {
+    const res = await getListTxByTxHashes(txHashes);
+    if (Array.isArray(res?.data)) {
+      txByTxHashes.value = res.data;
+    }
+  } catch (error) {
+    console.log({ error })
+  }
+}
 
-const variables = computed(() => {
-  return {
-    filter: {
-      id: { in: txHashes.value }
-    },
-  };
-});
-
-const { result } = useQuery(query, variables);
+watch(() => txHashes.value, () => {
+  if (Array.isArray(txHashes.value))
+    fetchListTxByTxHashes(txHashes.value)
+})
 
 const txsMerge = computed(() => {
   const txsOptimal = transactions.value;
-  const txsIndexer = result.value?.transactions?.results;
 
   const data = txsOptimal?.map((txOptimal: any) => {
-    const searchTx = txsIndexer?.find((txIndexer: any) => txIndexer.id === txOptimal.id);
-    const messages = searchTx?.messages;
+    const searchTx = txByTxHashes.value?.find((txIndexer: any) => txIndexer.id === txOptimal.id);
+    console.log({ searchTx })
+    const transactionMessages = searchTx?.transactionMessages;
 
     return {
       ...txOptimal,
-      messages,
+      messages: {
+        nodes: transactionMessages
+      }
     };
   });
 
@@ -122,7 +113,7 @@ function handleNext() {
 <template>
   <TransactionTable :transactions="txsMerge" :chain="chain" :txTotal="txTotal" :limit="pagination.limit"
     :handlePagination="handlePagination" />
-  <div class="flex items-center justify-center w-full mt-5">
+  <div class="flex items-center justify-center w-full mt-5" v-if="!txTotal">
     <button class="flex justify-center items-center px-4 py-2 bg-base rounded-s-lg w-20 border-r-2 border-[#383B40]" v-on:click="handlePrevious">Previous</button>
     <button class="flex justify-center items-center px-4 py-2 bg-base rounded-e-lg w-20" v-on:click="handleNext">Next</button>
   </div>
