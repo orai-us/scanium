eb7ee273656306004f699bbb809f5e10ce70465f
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2025-04-29 15:32:30 +0700

    update decode msg with oraichain proto

--- src/components/blocks/BlockSocket.vue
+++ src/components/blocks/BlockSocket.vue
@@ -4,6 +4,8 @@ import { computed, onBeforeUnmount, ref } from 'vue';
 import { shortenTxHash } from '@/utils';
 import { useRoute } from 'vue-router';
 import { useWindowSize } from '@vueuse/core';
+import { cosmos } from "@oraichain/proto/dist/codegen/cosmos/bundle";
+import { cosmwasm } from "@oraichain/proto/dist/codegen/cosmwasm/bundle";
 
 const props = defineProps(['chain']);
 const base = useBaseStore();
@@ -25,19 +27,39 @@ const list = computed(() => {
 
 const listTxs = computed(() => {
   const recents = base.txs;
-  const result = recents.map((item) => {
-    const msg = item?.tx_result?.events.find((event: any) => event?.type === "message" && event?.attributes.find((attr: any) => attr?.key === "action"));
-    const action = msg?.attributes?.find((attr: any) => attr?.key === "action");
-    const value = action?.value ?? 'Unknown';
-    return {
-      ...item,
-      message: value
-    }
-  });
+  try {
+    const result = recents.map((item) => {
+      const decodeTx = cosmos.tx.v1beta1.Tx.decode(Buffer.from(item.tx, 'base64'));
+      const messages = decodeTx?.body?.messages;
+      const splitMessage = messages?.[0]?.typeUrl?.split(".") ?? ['Unknown'];
+      const message = splitMessage[splitMessage.length - 1];
 
-  return result.sort(
-    (a, b) => Number(b?.height) - Number(a?.height)
-  );
+      // if (messages?.[0]?.typeUrl === "/cosmwasm.wasm.v1.MsgExecuteContract") {
+      //   const decodeValue = cosmwasm.wasm.v1.MsgExecuteContract.decode(messages[0].value);
+      //   const bufferValue = Buffer.from(decodeValue.msg).toString();
+      //   const parsedMsg = JSON.parse(bufferValue);
+
+      //   for (const key in parsedMsg) {
+      //     if (parsedMsg.hasOwnProperty(key)) {
+      //       const type = key.charAt(0).toUpperCase() + key.slice(1);
+      //       message = message + '/' + type;
+      //     }
+      //   }
+      // }
+
+      return {
+        ...item,
+        message,
+        messageLength: messages?.length ?? 0
+      }
+    });
+    return result.sort(
+      (a, b) => Number(b?.height) - Number(a?.height)
+    );
+  } catch (error) {
+    console.log({ catch: error });
+    return [];
+  }
 });
 
 onBeforeUnmount(() => {
@@ -95,10 +117,11 @@ function changeTab(selectedTab: string) {
       v-if="list.length > 0 && tab === 'blocks'">
       <div class="tabs tabs-boxed customTabV2 bg-transparent mb-4 p-6 pb-0" v-if="!isMobile">
         <a class="tab text-gray-400 capitalize !pb-3" :class="{ 'tab-active': tab === 'blocks' }">{{ $t('block.recent')
-        }}</a>
+          }}</a>
       </div>
 
       <div class="grid grid-cols-1 gap-3">
+        <!-- Mobile -->
         <div v-if="isMobile" v-for="item in list.slice(0, 10)" :index="item?.height" class="hover:bg-base-300">
           <div class="p-4 border border-[#383B40] rounded-lg flex flex-col gap-2">
             <span class="text-white text-sm font-bold">
@@ -123,10 +146,12 @@ function changeTab(selectedTab: string) {
               <span> {{ $t('account.time') }}</span>
               <span class="text-white text-sm font-bold"> {{
                 format.getRelativeTimeString(item?.time?.toString())
-                }}</span>
+              }}</span>
             </div>
           </div>
         </div>
+
+        <!-- Desktop -->
         <div class="bg-base-100 overflow-x-auto w-full rounded-2xl mb-4 px-5" v-if="!isMobile">
           <table class="table w-full table-compact">
             <thead class="border border-base-200">
@@ -150,7 +175,7 @@ function changeTab(selectedTab: string) {
                   </RouterLink>
                 </td>
                 <td class="text-sm text-link">
-                  <div class="mt-2 text-sm text-white font-semibold">
+                  <div class="mt-2 text-sm text-white font-semibold fixed-height">
                     <span>{{
                       format.validator(
                         item?.proposer_address &&
@@ -186,13 +211,14 @@ function changeTab(selectedTab: string) {
       </div>
 
       <div class="grid grid-cols-1 gap-3">
+        <!-- Mobile -->
         <div v-if="isMobile" v-for="item in listTxs.slice(0, 10)" :index="item?.height" class="hover:bg-base-300">
           <div class="p-4 border border-[#383B40] rounded-lg flex flex-col gap-2">
             <span class="text-white text-sm font-bold">
               <RouterLink :to="`/Oraichain/tx/${item?.hash}`" class="text-link cursor-pointer hover:text-primary">
-                    {{ shortenTxHash(item?.hash) }}
-                  </RouterLink>
-              </span>
+                {{ shortenTxHash(item?.hash) }}
+              </RouterLink>
+            </span>
             <div class="flex flex-row justify-between gap-2">
               <span>{{ $t('tx.message') }}</span>
               <span class="text-white text-sm font-bold"> {{ format.messages([{
@@ -207,11 +233,12 @@ function changeTab(selectedTab: string) {
               <span> {{ $t('account.time') }}</span>
               <span class="text-white text-sm font-bold"> {{
                 format.getRelativeTimeString(item?.timestamp?.toString())
-                }}</span>
+              }}</span>
             </div>
           </div>
         </div>
 
+        <!-- Desktop -->
         <div class="bg-base-100 overflow-x-auto w-full rounded-2xl mb-4 px-5" v-if="!isMobile">
           <table class="table w-full table-compact">
             <thead class="border border-base-200">
@@ -247,7 +274,7 @@ function changeTab(selectedTab: string) {
                 <td class="text-sm text-link">
                   <RouterLink :to="`/Oraichain/block/${item?.height}`">{{
                     item?.height
-                    }}</RouterLink>
+                  }}</RouterLink>
                 </td>
                 <td class="truncate text-right">
                   <span class="rounded text-xs whitespace-nowrap font-normal text-white text-right">
