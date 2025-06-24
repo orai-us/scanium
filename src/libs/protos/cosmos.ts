e9f6727b0621e028a3c4190a2bcaaf6085bd2dd5
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2025-06-24 14:08:21 +0700

    upadate name cosmos proto

--- src/libs/protos/cosmos.ts
+++ src/libs/protos/cosmos.ts
@@ -1,15 +1,15 @@
-import * as _137 from '@oraichain/proto/dist/codegen/cosmos/evm/vm/v1/tx';
-import * as _138 from '@oraichain/proto/dist/codegen/cosmos/evm/vm/v1/evm';
-import * as _139 from '@oraichain/proto/dist/codegen/cosmos/evm/vm/v1/genesis';
+import * as _415 from '@oraichain/proto/dist/codegen/cosmos/evm/vm/v1/tx';
+import * as _416 from '@oraichain/proto/dist/codegen/cosmos/evm/vm/v1/evm';
+import * as _417 from '@oraichain/proto/dist/codegen/cosmos/evm/vm/v1/genesis';
 
 export default {
   cosmos: {
     evm: {
       vm: {
         v1: {
-          ..._137,
-          ..._138,
-          ..._139,
+          ..._415,
+          ..._416,
+          ..._417,
         },
       },
     },
