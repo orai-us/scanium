8b75d7fe9b5f3607c59b9682fa03e2dde2084d01
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2025-07-15 11:19:30 +0700

    sort list txs rpc

--- src/components/account/TransactionAccountRpc.vue
+++ src/components/account/TransactionAccountRpc.vue
@@ -6,18 +6,26 @@ import { ref, watch } from 'vue';
 
 const props = defineProps(['address', 'chain', 'txs']);
 const format = useFormatter();
-const transactions = ref([] as Array<any>)
+const transactions = ref([] as Array<any>);
 
 watch([() => props.txs], () => {
-  transactions.value = props.txs?.map((item: any) => ({
-    txhash: toHex(item?.hash),
-    result: "Success",
-    message: format.messages(item.txRaw.body.messages),
-    height: item?.height,
-    fee: item?.fee || 0,
-    timestamp: format.toDay(item?.timestamp, 'from')
-  }));
-})
+  transactions.value = props.txs?.map((item: any) => {
+    let fee = item?.fee;
+    if (!fee) {
+      fee = item?.txRaw?.authInfo?.fee?.amount[0];
+    }
+    return {
+      txhash: toHex(item?.hash),
+      result: 'Success',
+      message: format.messages(item.txRaw.body.messages),
+      height: item?.height,
+      fee,
+      timestamp: format.toDay(item?.timestamp, 'from'),
+    };
+  });
+
+  transactions.value.sort((a, b) => b.height - a.height);
+});
 </script>
 <template>
   <TransactionTable :transactions="transactions" :chain="chain" />
